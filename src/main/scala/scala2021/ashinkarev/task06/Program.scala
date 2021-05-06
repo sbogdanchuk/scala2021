283ff85de7c606fd740276886ff4cce42d0f5186
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-06 17:42:38 +0500

    cleanup: remove copied comment

--- src/main/scala/scala2021/ashinkarev/task06/Program.scala
+++ src/main/scala/scala2021/ashinkarev/task06/Program.scala
@@ -3,7 +3,6 @@ package scala2021.ashinkarev.task06
 case class Form(name: String, age: Int, email: String)
 
 object Program extends App {
-  // Найти имя менеджера департамента, в котором работает сотрудник по имени сотрудника
   def isValidName(name: String): Either[String, Boolean] = {
     return NameValidator.isValidName(name);
   }
