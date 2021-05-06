bdcf11992533aa2f8586a300c10ce539b510f857
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-06 17:57:06 +0500

    chore(task06): add EmailValidator.scala

---
+++ src/main/scala/scala2021/ashinkarev/task06/EmailValidator.scala
@@ -0,0 +1,14 @@
+package scala2021.ashinkarev.task06
+
+object EmailValidator {
+  // https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+  private val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
+
+  def isEmailValid(email: String): Either[String, Boolean] = {
+    email match {
+      case email if email.isEmpty() => Right(true)
+      case email if emailRegex.findFirstMatchIn(email).isDefined => Right(true)
+      case _ => Left(s"Email is invalid.")
+    }
+  }
+}
