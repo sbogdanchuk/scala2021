3152ecf7778322ffa4c8d40469915c6f063f3ecd
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-06 16:40:27 +0500

    test(task06): add the rest of name validation cases

--- src/test/scala/scala2021/ashinkarev/task06/ProgramSuite.scala
+++ src/test/scala/scala2021/ashinkarev/task06/ProgramSuite.scala
@@ -13,4 +13,16 @@ class ProgramSuite extends AnyFunSuite with TableDrivenPropertyChecks with Scala
   test("isValidName for empty string => true") {
     isValidName("") should be (Right(true))
   }
+
+  test("isValidName for non-latin letters => returns incorrect symbols") {
+    isValidName("цыц") should be (Left(s"Only latin letters are allowed. Those are not allowed symbols: ц ы."))
+    isValidName("123") should be (Left(s"Only latin letters are allowed. Those are not allowed symbols: 1 2 3."))
+    isValidName("abcцыцops123zz") should be (Left(s"Only latin letters are allowed. Those are not allowed symbols: ц ы 1 2 3."))
+    isValidName("abc  a") should be (Left(s"Only latin letters are allowed. Those are not allowed symbols: whitespace."))
+    isValidName(", ^$") should be (Left(s"Only latin letters are allowed. Those are not allowed symbols: , whitespace ^ $$ ."))
+  }
+
+  test("isValidName for latin letters of any case => true") {
+    isValidName("Zzzabc") should be (Right(true))
+  }
 }
