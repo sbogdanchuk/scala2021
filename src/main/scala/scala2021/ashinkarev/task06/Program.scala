64174cd769a9f886a7556ed547c345400a33baa1
Aleksandr Shinkarev <pl.development@nielsen.com>
2021-05-06 18:13:45 +0500

    chore(task06): add sex and height to Form

--- src/main/scala/scala2021/ashinkarev/task06/Program.scala
+++ src/main/scala/scala2021/ashinkarev/task06/Program.scala
@@ -1,6 +1,14 @@
 package scala2021.ashinkarev.task06
 
-case class Form(name: String, age: Int, email: String)
+import scala2021.ashinkarev.task06.Sex._
+
+case class Form(
+  name: String,
+  age: Int, 
+  email: String, 
+  sex: Sex,
+  height: Double
+)
 
 object Program extends App {
   def isValidName(name: String): Either[String, Boolean] = {
---
+++ src/main/scala/scala2021/ashinkarev/task06/Sex.scala
@@ -0,0 +1,6 @@
+package scala2021.ashinkarev.task06
+
+object Sex extends Enumeration {
+  type Sex = Value
+  val Male, Female = Value
+}
